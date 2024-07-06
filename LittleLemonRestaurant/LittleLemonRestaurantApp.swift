5fdac7e5f1f1900195e3b558cfd26869b9f59831
Santosh Acharya <santoshacharya747@gmail.com>
2024-07-06 16:13:01 +0545

    Initial Commit

---
+++ LittleLemonRestaurant/LittleLemonRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonRestaurantApp.swift
+//  LittleLemonRestaurant
+//
+//  Created by Santosh Acharya on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
